46cfbe11591bde4649b121687e4bf66d46adb397
Cal Smith <calsmith@protonmail.com>
2019-10-02 14:25:51 -0400

    fix(breadcrumb): add router support

--- src/breadcrumb/breadcrumb-item.component.ts
+++ src/breadcrumb/breadcrumb-item.component.ts
@@ -1,17 +1,22 @@
 import {
 	Component,
 	HostBinding,
-	Input
+	Input,
+	Optional,
+	EventEmitter,
+	Output
 } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
 	selector: "ibm-breadcrumb-item",
 	template: `
 	<a
+		*ngIf="skeleton || href; else content"
 		class="bx--link"
-		href="{{skeleton ? '/#' : href}}"
+		href="{{skeleton ? '#' : href}}"
 		[attr.aria-current]="(current ? ariaCurrent : null)"
-		*ngIf="skeleton || href; else content">
+		(click)="navigate($event)">
 		<ng-container *ngTemplateOutlet="content"></ng-container>
 	</a>
 	<ng-template #content>
@@ -25,7 +30,34 @@ export class BreadcrumbItemComponent {
 
 	@Input() ariaCurrent = "page";
 
+	/**
+	 * Array of commands to send to the router when the link is activated
+	 * See: https://angular.io/api/router/Router#navigate
+	 */
+	@Input() route: any[];
+
+	/**
+	 * Router options. Used in conjunction with `route`
+	 * See: https://angular.io/api/router/Router#navigate
+	 */
+	@Input() routeExtras: any;
+
+	/**
+	 * Emits the navigation status promise when the link is activated
+	 */
+	@Output() navigation = new EventEmitter<Promise<boolean>>();
+
 	@HostBinding("class.bx--breadcrumb-item--current") @Input() current = false;
 
 	@HostBinding("class.bx--breadcrumb-item") itemClass = true;
+
+	constructor(@Optional() protected router: Router) { }
+
+	navigate(event) {
+		if (this.router && this.route) {
+			event.preventDefault();
+			const status = this.router.navigate(this.route, this.routeExtras);
+			this.navigation.emit(status);
+		}
+	}
 }
